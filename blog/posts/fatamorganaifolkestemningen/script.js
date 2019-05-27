96a8c013c3b131010d4f0487b4a34ceccde8df6a
Felix Sebastian Døssing <felixdoessing@gmail.com>
2019-05-27 19:19:58 +0200

    Update script.js

--- blog/posts/fatamorganaifolkestemningen/script.js
+++ blog/posts/fatamorganaifolkestemningen/script.js
@@ -32,22 +32,37 @@ data = {
     },
   },
   eurobarometer:{
-    title:"Syn på indvandring fra lande udenfor EU",
-    question:"Fortæl hvorvidt følgende sætning vækker en positive eller negativ følelse hos dig: Immigration af folk fra udenfor EU",
-    data: {
-      answers:["Meget positiv","Rimelig positiv", "Rimelig negative", "Meget negative", "Ved ikke"],
-      years:["2014", "2015a", "2015b","2016a","2016b","2017a","2017b","2018a","2018b"],
-      response:[
-        [7.37,	34.86,	32.37,	19.42,	5.98],
-        [4.31,	29.41,	38.63,	17.94,	9.71],
-        [4.50,	24.90,	35.80,	25.90,	8.90],
-        [2.78,	27.01,	38.03,	24.73,	7.45],
-        [5.67,	24.78,	40.10,	23.08,	6.37],
-        [4.72,	25.59,	39.17,	22.83,	7.68],
-        [4.20,	25.80,	38.50,	23.60,	7.90],
-        [4.83,	29.56,	38.13,	19.61,	7.88],
-        [4.12,	25.61,	37.68,	24.14,	8.44]
-      ]
+    noneuimmigration:{
+      title:"Syn på indvandring fra lande udenfor EU",
+      question:"Fortæl hvorvidt følgende sætning vækker en positive eller negativ følelse hos dig: Immigration af folk fra udenfor EU",
+      data: {
+        answers:["Meget positiv","Rimelig positiv", "Rimelig negative", "Meget negative", "Ved ikke"],
+        years:["08-11-2014","16-05-2015","07-11-2015","21-05-2016","03-11-2016","20-05-2017","05-11-2017","17-03-2018","08-11-2018"],
+        response:[
+          [7.37,	34.86,	32.37,	19.42,	5.98],
+          [4.31,	29.41,	38.63,	17.94,	9.71],
+          [4.50,	24.90,	35.80,	25.90,	8.90],
+          [2.78,	27.01,	38.03,	24.73,	7.45],
+          [5.67,	24.78,	40.10,	23.08,	6.37],
+          [4.72,	25.59,	39.17,	22.83,	7.68],
+          [4.20,	25.80,	38.50,	23.60,	7.90],
+          [4.83,	29.56,	38.13,	19.61,	7.88],
+          [4.12,	25.61,	37.68,	24.14,	8.44]
+        ]
+      }
+    },
+    mostimportantissue:{
+      title:"Det to vigtigste problemer DK står overfor",
+      quetsion:"Hvad mener du er de to vigtigste problemer, som Danmark står overfor?",
+      data: {
+        years:["09-05-2005","11-10-2005","06-09-2006","10-04-2007","22-09-2007","25-03-2008",
+               "06-10-2008","12-06-2009","23-10-2009","05-05-2010","12-05-2012","03-11-2012",
+               "10-05-2013","02-11-2013","15-03-2014","31-05-2014","08-11-2014","28-02-2015",
+               "16-05-2015","07-11-2015","21-05-2016","03-11-2016","20-05-2017","05-11-2017",
+               "17-03-2018","08-11-2018"],
+        response:[30.79,27.08,28.58,21.38,22.12,18.35,17.2,9.88,15.48,9.54,9.23,9.47,10.31,17.08,20.83,
+                  20.24,34.2,31.69,35.44,60.06,57.11,41.24,37.63,32.05,34.32,29.98]
+      }
     }
   }
 }
