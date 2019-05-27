a1e1ca55876959ff955f7916d4ad5959dfebf15c
Felix Sebastian Døssing <felixdoessing@gmail.com>
2019-05-27 18:55:19 +0200

    Update script.js

--- blog/posts/fatamorganaifolkestemningen/script.js
+++ blog/posts/fatamorganaifolkestemningen/script.js
@@ -14,3 +14,21 @@ $(document).on('scroll',function() {
 
 
 });
+
+
+/// Graph stuff
+
+data = {
+  electionstudy: {
+    muslims:{
+      title:"Muslimsk indvandring udgør trussel for DK",
+      question:"De muslimske lande udgør på længere sigt en farlig trussel mod Danmarks sikkerhed",
+      data:{ answers:["Enig", "Uenig"], years:[1990,1994,1998,2001,2005,2007], response[[68,51,43,43,45,48],[22,40,40,42,38,32]] }
+    },
+    sovereignty:{
+      title:"Indvandring udgør trussel mod national egenart",
+      question:"Indvandring udgør en alvorlig trussel mod vores nationale egenart",
+      data:{ answers:["Enig", "Uenig"], years:[1988,1990,1994,1998,2001,2005,2007,2011,2015], response[[48,47,48,45,45,45,46,53,44],[48,43,47,42,40,41,34,31,41]] }
+    },
+  }
+}
